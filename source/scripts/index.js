81db3a83fc402833d9f4ff06172eb376fb429a35
Lonelypactyx <41754883+Lonelypactyx@users.noreply.github.com>
2024-09-13 23:53:21 +0300

    Module6-7-catalog (#12)
    * сделан footer и добавлена карта
    
    * catalog основная продукция
    
    * other-product
    
    * giftbox
    
    * subscribe
    
    * webp preload font

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -55,4 +55,15 @@ function compareSlider(i1, i2, btn) {
   }
 }
 
-compareSlider(img1, img2, comparebtn);
+const img1 = document.querySelector('.results-slider__img-before');
+const img2 = document.querySelector('.results-slider__img-after');
+const comparebtn = document.querySelector('.results-slider__btn');
+
+if (document.querySelector('.results-slider')) {
+  compareSlider(img1, img2, comparebtn);
+}
+
+// яндекс карта
+
+/* const map = document.querySelector('.main-footer__where > picture');
+map.outerHTML = '<iframe src="https://yandex.by/map-widget/v1/?ll=30.330282%2C59.938081&mode=search&oid=24933578998&ol=biz&sctx=ZAAAAAgBEAAaKAoSCQiPNo5Y4FhAEXbCS3Dq205AEhIJbmsLz0uDZUARRUlIpG3gR0AiBgABAgMEBSgKOABA5a4HSAFiSHJlYXJyPXNjaGVtZV9Mb2NhbC9HZW8vTWVkaWFGbG93L1N0b3JpZXNDb250ZW50VHlwZT1uZXdfbWVkaWFfY2xhc3NpZmllcmoCdWGdAc3MTD2gAQCoAQC9AQzVivXCAQXj6bjkBIICDGh0bWwgYWNhZGVteYoCAJICAJoCDGRlc2t0b3AtbWFwc6oCaTE3NDU4NzE1NDYyNCwxOTc2NDMyMTQwNjcsMTE3Nzk2NTk3ODksNzY3NTg0NjQzMDUsMjI4NDkwNDU2MzIyLDE3MjU5MzM0MDMwLDkyMzIyMzg3ODMsNzMyMTg1NzM4LDczMjE4NTczN7ACAQ%3D%3D&sll=30.330282%2C59.938081&source=serp_navig&sspn=0.063340%2C0.016251&text=html%20academy&z=14.64" width="100%" height="400" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe>'; */
