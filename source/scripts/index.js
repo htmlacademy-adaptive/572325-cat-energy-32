c2b2c3c663ecbeb43e773dbbe4376dcc79d59403
Lonelypactyx <41754883+Lonelypactyx@users.noreply.github.com>
2024-09-17 03:04:16 +0300

    Module10 (#13)
    * hover focus active
    
    * pixelperfect

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,15 +1,3 @@
-/* document.addEventListener('DOMContentLoaded', () => {
- const btn = document.querySelector('.main-nav__toggle--open');
-  btn.classList.remove('main-nav__toggle--open');
-  document.documentElement.addEventListener('resize', () => {
-    if(this.clientWidth < 768) {
-      btn.style.display = 'grid !important';
-    } else {
-      btn.classList.remove('main-nav__toggle--open');
-    }
-  });
-}); */
-
 const nav = document.querySelector('.main-nav__list');
 nav.insertAdjacentHTML('beforeBegin', '<button class="main-nav__toggle" type="button" onclick="this.classList.toggle(\'main-nav__toggle--open\')"><div class="main-nav__toggle-line"></div><span class="visually-hidden">Открыть меню</span></button>');
 nav.classList.remove('no-js');
@@ -63,7 +51,7 @@ if (document.querySelector('.results-slider')) {
   compareSlider(img1, img2, comparebtn);
 }
 
-// яндекс карта
+// google карта
 
-/* const map = document.querySelector('.main-footer__where > picture');
-map.outerHTML = '<iframe src="https://yandex.by/map-widget/v1/?ll=30.330282%2C59.938081&mode=search&oid=24933578998&ol=biz&sctx=ZAAAAAgBEAAaKAoSCQiPNo5Y4FhAEXbCS3Dq205AEhIJbmsLz0uDZUARRUlIpG3gR0AiBgABAgMEBSgKOABA5a4HSAFiSHJlYXJyPXNjaGVtZV9Mb2NhbC9HZW8vTWVkaWFGbG93L1N0b3JpZXNDb250ZW50VHlwZT1uZXdfbWVkaWFfY2xhc3NpZmllcmoCdWGdAc3MTD2gAQCoAQC9AQzVivXCAQXj6bjkBIICDGh0bWwgYWNhZGVteYoCAJICAJoCDGRlc2t0b3AtbWFwc6oCaTE3NDU4NzE1NDYyNCwxOTc2NDMyMTQwNjcsMTE3Nzk2NTk3ODksNzY3NTg0NjQzMDUsMjI4NDkwNDU2MzIyLDE3MjU5MzM0MDMwLDkyMzIyMzg3ODMsNzMyMTg1NzM4LDczMjE4NTczN7ACAQ%3D%3D&sll=30.330282%2C59.938081&source=serp_navig&sspn=0.063340%2C0.016251&text=html%20academy&z=14.64" width="100%" height="400" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe>'; */
+const map = document.querySelector('.main-footer__where > picture');
+map.outerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1996.8224469756535!2d30.314833975887662!3d59.96826865971507!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4696310fca5afc31%3A0x5f2ac55f567b2400!2sHTML%20Academy!5e0!3m2!1sru!2sby!4v1726530691374!5m2!1sru!2sby" width="100%" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
